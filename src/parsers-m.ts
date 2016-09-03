51101799d392ec5f438b0777521911a51bfdc568
toby <sugto555@gmail.com>
2016-09-03 16:07:27 +1200

    Get new parser style with map working. Tidy up.

--- src/parsers-m.ts
+++ src/parsers-m.ts
@@ -5,120 +5,10 @@ import * as util from './util';
 export type NoResult = null;
 export const noResult: NoResult = null;
 
-class MyParser {
-  c: string;
-  input: Input;
-
-  constructor(c: string, input: Input) {
-    this.c = c;
-    this.input = input;
-  }
-
-  then(f: Function) {
-    let r = this.input.nextChar();
-
-    if (r === this.c) {
-      this.input.advance();
-      // return this.c;
-      f(this.c);
-    }
-    else {
-      f(noResult);
-      // return noResult;
-    }
-  }
-}
-
-function char4(c: string) {
-  return (input: Input) => {
-    return new MyParser(c, input);
-  };
-}
-
-interface LeParser {
-  apply: (Input) => LeParser;
-  then: (Function) => LeParser;
-}
-
-export function char(c: string): LeParser {
-  let result: string | NoResult = noResult;
-
-  let self: LeParser = {
-    apply: (input: Input) => {
-      let r = input.nextChar();
-
-      if (r === c) {
-        input.advance();
-        result = c;
-      }
-      return self;
-    },
-    then: (f: Function) => {
-      f(result);
-      return self;
-    }
-  };
-
-  return self;
-}
-
-export function char3(c: string) {
-  return (input: Input) => {
-
-    let self = {
-      then: (f: Function) => {
-        let r = input.nextChar();
-
-        if (r === c) {
-          input.advance();
-          f(c);
-        }
-        else {
-          f(noResult);
-        }
-        return self
-      }
-    };
-
-    return self;
-  }
-}
 
 interface IParser {
   apply: (Input) => any;
   map: (Function) => IParser;
-}
-
-export function char5(c: string): IParser {
-  let mapFunc: Function;
-
-  let self = {
-    apply: (input: Input) => {
-      let r = input.nextChar();
-
-      let result: any | null = noResult;
-
-      if (r === c) {
-        input.advance();
-        result = c;
-      }
-
-      if (mapFunc) {
-        return mapFunc(result);
-      }
-      else {
-        return result;
-      }
-    },
-
-    map: (f: Function) => {
-      mapFunc = f;
-
-      return self;
-    },
-  };
-
-  return self;
 }
 
 function mkParser(applyFunc): IParser {
@@ -147,7 +37,7 @@ function mkParser(applyFunc): IParser {
   return self;
 }
 
-function char7(c: string): IParser {
+function char(c: string): IParser {
   return mkParser((input: Input, handleResult) => {
     let r = input.nextChar();
 
@@ -162,103 +52,35 @@ function char7(c: string): IParser {
   });
 }
 
+export function and(p1: IParser, p2: IParser) {
+  return mkParser((input: Input, handleResult) => {
+    const pos = input.getPosition();
 
-abstract class CParser {
-  mapFunc: Function;
+    const r1 = applyParser2(p1, input);
+    if (r1 === noResult) {
+      input.setPosition(pos);
 
-  abstract apply(input: Input): CParser;
-
-  map(f: Function): CParser {
-    this.mapFunc = f;
-
-    return this;
-  }
-
-  getResult(result) {
-    if (this.mapFunc) {
-      return this.mapFunc(result);
+      return noResult;
     }
-    else {
-      return result;
+
+    const r2 = applyParser2(p2, input);
+    if (r2 === noResult) {
+      input.setPosition(pos);
+
+      return noResult;
     }
-  }
+    return handleResult([r1, r2].filter(r => r !== ''));
+  });
 }
 
-class Char extends CParser {
-  c: string;
-
-  constructor(c) {
-    super();
-    this.c = c;
-  }
-
-  apply(input: Input): CParser {
-    let r = input.nextChar();
-
-    let result: any | null = noResult;
-
-    if (r === this.c) {
-      input.advance();
-      result = this.c;
-    }
-
-    return this.getResult(result);
-  }
-}
-
-
-export function char2(c: string) {
-  return (input: Input) => new Promise((resolve, reject) => {
-    let r = input.nextChar();
-
-    if (r === c) {
-      input.advance();
-      resolve(c);
-    }
-    resolve(undefined);
-  })
-}
-
-export function and(p1: LeParser, p2: LeParser) {
-  let result: any | NoResult = noResult;
-
-  let self: LeParser = {
-    apply: (input: Input) => {
-      const pos = input.getPosition();
-
-      applyParser(p1, input).then((r1) => {
-        if (r1 === noResult) {
-          input.setPosition(pos);
-        }
-        else {
-          applyParser(p2, input).then((r2) => {
-            if (r2 === noResult) {
-              input.setPosition(pos);
-            }
-            else {
-              result = [r1, r2].filter(r => r !== '');
-            }
-          });
-        }
-      });
-
-      return self;
-    },
-    then: (f: Function) => {
-      f(result);
-      return self;
-    }
-  };
-
-  return self;
-}
 
 function testPromises() {
-  const c = char7('c').map(r => console.log(r));
-  const input = new Input('c');
+  const c = char('c').map(r => console.log(r));
+  const input = new Input('bc');
 
+  const bc = and(char('b'), char('c').map(c => c)).map((r: string[]) => {return r.join('')});
 
-  const result = applyParser2(c, input);
+  const result = applyParser2(bc, input);
 
   console.log('result: ', result);
 }
@@ -266,9 +88,5 @@ testPromises();
 
 
 export function applyParser2(parser, input) {
-  return parser.apply(input);
-}
-
-export function applyParser(parser: LeParser, input: Input): LeParser {
   return parser.apply(input);
 }
