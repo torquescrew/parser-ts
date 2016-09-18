15a8e12ea9e2f8c51ab083aa474af90a594c55c2
toby <sugto555@gmail.com>
2016-09-18 14:37:12 +1200

    Tidy up.

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,10 @@
 var assert = require('assert');
-
 import {expr} from "../src/infix-lang";
 import {parse} from '../src/parser-lib/parsers-m';
 import {ETypes} from "../src/infix-lang/expr-types/expr";
 
 
-describe('expr parser should output correct type', function() {
-
+describe('Simple expressions:', function() {
   checkExprParse(ETypes.VariableDefinition, 'let a = 5');
   checkExprParse(ETypes.Boolean, 'true');
   checkExprParse(ETypes.Number, '3');
@@ -14,7 +12,18 @@ describe('expr parser should output correct type', function() {
   checkExprParse(ETypes.String, '"hi there"');
   checkExprParse(ETypes.Identifier, 'squareNum2');
   checkExprParse(ETypes.FunctionCall, 'a..times(2)');
+});
 
+describe('Conditionals:', function() {
+  checkExprParse(ETypes.Conditional, 'if a { let b = a }');
+  checkExprParse(ETypes.Conditional, 'if a { let a = 5 } else if b { let b = c } else { let a = 3 }');
+});
+
+describe('Operators:', function() {
+  checkExprParse(ETypes.Operators, '5 * 5');
+  checkExprParse(ETypes.Operators, '5 * 5 + 3');
+  checkExprParse(ETypes.Operators, '(5 / 5) + 3 - 5');
+  // checkExprParse(ETypes.Operators, '(myFunc(5, 3) / 5) + 3 - 5');
 });
 
 
@@ -22,27 +31,7 @@ function checkExprParse(type: string, code: string): void {
   describe(`"${code}"`, function() {
     it(`${type}`, function() {
       const result = parse(expr, code);
-      // console.log(JSON.stringify(result, null, 3));
       assert.equal(result['type'], type);
     });
   });
 }
-
-describe('conditionals', function() {
-  checkExprParse(ETypes.Conditional, 'if a { let b = a }');
-  checkExprParse(ETypes.Conditional, 'if a { let a = 5 } else if b { let b = c } else { let a = 3 }');
-
-  // it('parses', function() {
-  //   const result = parse(expr, 'if a { let a = 5 } else if b { } else { let a = 3 }');
-  //   console.log(result);
-  // });
-});
-
-describe('operators', function() {
-  checkExprParse(ETypes.Operators, '5 * 5');
-  checkExprParse(ETypes.Operators, '5 * 5 + 3');
-  checkExprParse(ETypes.Operators, '(5 / 5) + 3 - 5');
-
-});
-
-
